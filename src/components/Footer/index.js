831fe7bf56c5188fa4383bc79eb6a4d8ad2e0c79
ispadilha <israel.padilha@gmail.com>
2023-08-08 05:38:31 -0300

    Created Banner component, renamed images

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Desenvolvido com Alura</h2>
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,14 @@
+import Banner from "components/Banner"
+import Footer from "components/Footer"
 import Header from "components/Header"
 
 function Home(){
     return (
-        <Header />
+        <>
+            <Header />
+            <Banner image="home" />
+            <Footer />
+        </>
     )
 }
 
