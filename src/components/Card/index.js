9c3af79e249bf3853d619453dc1ff0861447d5a2
ispadilha <israel.padilha@gmail.com>
2023-08-14 11:59:15 -0300

    Added favorite icon function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,19 @@
 import { useFavoriteContext } from "contexts/FavoritesContext"
 import styles from "./Card.module.css"
 import iconFavOutline from "./favorite_outline.png"
+import iconFavFilled from "./favorite.png"
 
 function Card({ id, title, cover }) {
     const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id)
+    const icon = isFavorite? iconFavFilled : iconFavOutline
     return (
         <div className={styles.container}>
             <img src={cover} alt={title} className={styles.cover} />
             <h2>{title}</h2>
-            <img src={iconFavOutline} alt="Favoritar filme" className={styles.favOutline}
-                onClick={() => {
-                    addFavorite({ id, title, cover })
-                }} />
+            <img src={icon} alt="Favoritar filme" className={styles.favOutline}
+                onClick={() => { addFavorite({ id, title, cover }) }}
+            />
         </div>
     )
 }
