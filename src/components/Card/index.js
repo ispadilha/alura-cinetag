3be6a9eda2c3f7fad8cf033b149ff10e4008ccea
ispadilha <israel.padilha@gmail.com>
2023-08-14 10:52:38 -0300

    Added favorite icons onClick function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,17 @@
+import { useFavoriteContext } from "contexts/FavoritesContext"
 import styles from "./Card.module.css"
 import iconFavOutline from "./favorite_outline.png"
 
 function Card({id, title, cover}){
+    const {favorite, addFavorite} = useFavoriteContext()
     return(
         <div className={styles.container}>
             <img src={cover} alt={title} className={styles.cover}/>
             <h2>{title}</h2>
-            <img src={iconFavOutline} alt="Favoritar filme" className={styles.favOutline}/>
+            <img src={iconFavOutline} alt="Favoritar filme" className={styles.favOutline}
+            onClick={() => {
+                addFavorite({ id, title, cover })
+            }} />
         </div>
     )
 }
