7ee7f7f9306a24577059ea19ca2ef0ab1515f635
ispadilha <israel.padilha@gmail.com>
2023-08-14 10:22:52 -0300

    Imported favorites context in routes.js

---
+++ src/contexts/FavoritesContext.js
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+
+export const FavoritesContext = createContext()
+FavoritesContext.displayName = "Favoritos"
+
+export default function FavoritesProvider({ children }) {
+    const [favorite, setFavorite] = useState([])
+
+    return (
+        <FavoritesContext.Provider
+            value={{ favorite, setFavorite }}>
+            {children}
+        </FavoritesContext.Provider>
+    )
+}
+
+export function useFavoriteContext() {
+    const { favorite, setFavorite } = useContext(FavoritesContext)
+
+    function addFavorite(newFavorite) {
+        const repeatedFavorite = favorite.some(item => item.id === newFavorite.id)
+
+        let newList = [...favorite]
+
+        if(!repeatedFavorite){
+            newList.push(newFavorite)
+            return setFavorite(newList)
+        }
+
+        novaLista = favorito.filter((fav) => fav.id !== novoFavorito.id);
+        return setFavorite(newList)
+    }
+    return {
+        favorite,
+        addFavorite
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Footer from "components/Footer"
 import Favorites from "pages/Favorites"
 import Home from "pages/Home"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import FavoritesProvider from "contexts/FavoritesContext"
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Header />
             <Container>
+                <FavoritesProvider>
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/favoritos" element={<Favorites />}></Route>
                 </Routes>
+                </FavoritesProvider>
             </Container>
             <Footer />
         </BrowserRouter>
