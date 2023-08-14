10471b13a3c40d8675b22dbd31a35b88602729ea
ispadilha <israel.padilha@gmail.com>
2023-08-14 11:25:33 -0300

    fix: identation error

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,16 +2,16 @@ import { useFavoriteContext } from "contexts/FavoritesContext"
 import styles from "./Card.module.css"
 import iconFavOutline from "./favorite_outline.png"
 
-function Card({id, title, cover}){
-    const {favorite, addFavorite} = useFavoriteContext()
-    return(
+function Card({ id, title, cover }) {
+    const { favorite, addFavorite } = useFavoriteContext()
+    return (
         <div className={styles.container}>
-            <img src={cover} alt={title} className={styles.cover}/>
+            <img src={cover} alt={title} className={styles.cover} />
             <h2>{title}</h2>
             <img src={iconFavOutline} alt="Favoritar filme" className={styles.favOutline}
-            onClick={() => {
-                addFavorite({ id, title, cover })
-            }} />
+                onClick={() => {
+                    addFavorite({ id, title, cover })
+                }} />
         </div>
     )
 }
